70f23ec267294569915addce3bc48eab7cd757f6
namanuma <koinuma442993@gmail.com>
2019-06-09 01:14:34 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 生沼一輝 on 2019/06/09.
+//  Copyright © 2019 生沼一輝. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
