30e16aa7b0fd33f527846abe03fa92b3f5cc1e33
namanuma <koinuma442993@gmail.com>
2019-06-09 01:42:24 +0900

    step1 section6

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 生沼一輝 on 2019/06/09.
+//  Copyright © 2019 生沼一輝. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 生沼一輝 on 2019/06/09.
+//  Copyright © 2019 生沼一輝. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
